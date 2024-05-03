a7afcb5d4da88a0946a202b4d45d7dedd0a00014
sophronia <sophronia.del@gmail.com>
2024-05-03 13:39:44 +0800

    change the way filter parameters are passed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace aoe3_auto_packager
+﻿using System.Collections.Immutable;
+using System.Linq;
+
+namespace aoe3_auto_packager
 {
     internal class Program
     {
@@ -9,28 +12,33 @@
             var begin = DateTime.Now;
             Console.WriteLine($"Start at ${DateTime.Now}");
 
-            Dictionary<string, string> arguments = [];
+            Dictionary<string, List<string>> arguments = [];
             foreach (var arg in args)
             {
                 var kv = arg.Split('=');
                 if (kv.Length == 2)
                 {
-                    arguments.Add(kv[0].Trim().ToLower(), kv[1].Trim());
+                    string key = kv[0].Trim().ToLower();
+                    List<string>? collector = arguments.GetValueOrDefault(key);
+                    if (collector == null)
+                    {
+                        collector = [];
+                        arguments.Add(key, collector);
+                    }
+                    collector.Add(kv[1].Trim());
                 }
             }
 
-            string sourceDir = arguments.GetValueOrDefault("source", "xml_data_source");
-            string dataDir = arguments.GetValueOrDefault("data", "Data");
-            string suffix = arguments.GetValueOrDefault("suffix", "generated");
-            string filter = arguments.GetValueOrDefault("filter", "");
+            string sourceDir = arguments.GetValueOrDefault("source", ["xml_data_source"])[0];
+            string dataDir = arguments.GetValueOrDefault("data", ["Data"])[0];
+            string suffix = arguments.GetValueOrDefault("suffix", ["generated"])[0];
 
-            ISet<string>? pathFilter = filter.Length == 0 ?
-                null :
-                filter.Split(",").
+            List<string> rawFilter = arguments.GetValueOrDefault("filter", []);
+            var pathFilter = rawFilter.
                 Select(s => Path.Join(sourceDir, s.Replace('/', '\\'))).
-                ToHashSet();
+                ToImmutableSortedSet();
 
-            Console.WriteLine($"Creating bar file based on [{dataDir}] with data source from [{sourceDir}]");
+            Console.WriteLine($"Creating bar file based on [{dataDir}] with data source from [{sourceDir}], using file filter [{string.Join(',', pathFilter)}]");
 
             List<string> files = [];
             CollectXmlFiles(sourceDir, files, pathFilter);
@@ -63,7 +71,7 @@
             Console.WriteLine($"Finished. Time Cost: {cost} ms");
         }
 
-        private static void CollectXmlFiles(string current, List<string> container, ISet<string>? pathFilter)
+        private static void CollectXmlFiles(string current, List<string> container, ISet<string> pathFilter)
         {
             string[] directDirectories = Directory.GetDirectories(current);
             foreach (var dir in directDirectories)
@@ -79,7 +87,7 @@
             string[] files = Directory.GetFiles(current);
             foreach(var file in files)
             {
-                if (pathFilter != null && !pathFilter.Contains(file))
+                if (pathFilter.Count > 0 && !pathFilter.Contains(file))
                 {
                     continue;
                 }
